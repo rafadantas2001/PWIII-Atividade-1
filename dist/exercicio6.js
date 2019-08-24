fe025c8fb544f6a90eb73b216ce6fc66e70ca302
Rafael Dantas <rafael.dantas2001@hotmail.com>
2019-08-23 21:57:02 -0300

    exercicio 6

---
+++ dist/exercicio6.js
@@ -0,0 +1,4 @@
+var calcular = function (largura, comprimento) {
+    return (largura * comprimento);
+};
+console.log(calcular(7, 3));
---
+++ exercicio6.ts
@@ -0,0 +1,6 @@
+let calcular = function(largura : number, comprimento : number) {
+
+    return(largura * comprimento)
+}
+
+console.log(calcular(7,3))
