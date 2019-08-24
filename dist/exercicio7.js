cf30dc079a247d0242d1c55e27aac94d5cabc584
Rafael Dantas <rafael.dantas2001@hotmail.com>
2019-08-23 22:06:28 -0300

    exercicio 7

---
+++ dist/exercicio7.js
@@ -0,0 +1,2 @@
+var cubo = function (numero1, numero2, numero3) { return numero1 * numero2 * numero3; };
+console.log(cubo(3, 3, 3));
---
+++ exercicio7.ts
@@ -0,0 +1,5 @@
+let cubo = (numero1:number,numero2:number,numero3 : number) => numero1*numero2*numero3
+
+console.log(cubo(3,3,3))
+
+
