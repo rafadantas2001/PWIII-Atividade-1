99608a5264625954e562b9a35b417d50112fa5d2
Rafael Dantas <rafael.dantas2001@hotmail.com>
2019-08-23 21:23:50 -0300

    execicio2-3

--- dist/exercicio2-3.js
+++ dist/exercicio2-3.js
@@ -1,1 +1,4 @@
 var salario = 1500.98;
+console.log(salario);
+var texto = "Sal\u00E1rio informado \u00E9 de R$ (" + salario + ")";
+console.log(texto);
--- exercicio2-3.ts
+++ exercicio2-3.ts
@@ -1,1 +1,4 @@
-let salario: number = 1500.98;
+let salario : number = 1500.98
+console.log(salario)
+let texto = `Salário informado é de R$ (${salario})`
+console.log(texto);
