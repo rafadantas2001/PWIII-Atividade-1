c6809044bc1f41ae6f2d18aaa2d47bae80bac5f4
Rafael Dantas <rafael.dantas2001@hotmail.com>
2019-08-23 22:33:46 -0300

    exercicio 8

---
+++ dist/exercicio8.js
@@ -0,0 +1,16 @@
+var Piscina = /** @class */ (function () {
+    function Piscina(diametro, custoConstrucao) {
+        this.diametro = diametro;
+        this.custoConstrucao = custoConstrucao;
+    }
+    Piscina.prototype.raio = function () {
+        return this.diametro / 2;
+    };
+    Piscina.prototype.area = function () {
+        return 3141592 * (this.raio() * this.raio());
+    };
+    Piscina.prototype.custoTotal = function () {
+        return this.area() * this.custoConstrucao;
+    };
+    return Piscina;
+}());
---
+++ exercicio8.ts
@@ -0,0 +1,23 @@
+class Piscina {
+constructor(public diametro : number,public custoConstrucao : number){}
+raio() {
+return this.diametro/2
+
+}
+area() {
+  return 3141592 * (this.raio() * this.raio())
+  
+}
+custoTotal() {
+
+    return this.area()*this.custoConstrucao
+}
+
+
+
+
+
+
+
+}
+
