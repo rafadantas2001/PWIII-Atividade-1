73c06fc698f29de80a680d94ac3ef96cd9734108
Rafael Dantas <rafael.dantas2001@hotmail.com>
2019-08-23 21:41:35 -0300

    exercicio4-5

---
+++ dist/exercicio4-5.js
@@ -0,0 +1,8 @@
+var numero = 21;
+var resto = numero % 2;
+if (resto == 0) {
+    console.log("par");
+}
+else {
+    console.log("ímpar");
+}
---
+++ exercicio4-5.ts
@@ -0,0 +1,10 @@
+let numero = 21;
+let resto = numero % 2;
+
+if(resto == 0){
+    console.log("par")
+
+}
+else{
+    console.log("ímpar")
+}
